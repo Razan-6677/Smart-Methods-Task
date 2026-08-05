b0ec39abd241e396f0610ff807e157ddde6dc80a
Razan-6677 <razank767@gmail.com>
2026-08-06 00:24:51 +0300

    Update and rename setup (1).sql to setup.sql

--- Web-Development/TEST/codes/CntrolLed1/setup.sql
+++ Web-Development/TEST/codes/CntrolLed1/setup.sql
@@ -1,4 +1,3 @@
--- شغّل الكود ده مرة واحدة في تبويب SQL في phpMyAdmin
 
 CREATE TABLE robot_state (
     id INT PRIMARY KEY,
@@ -6,5 +5,4 @@ CREATE TABLE robot_state (
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
--- إدخال الصف الوحيد اللي هيتحدث باستمرار (id = 1)
 INSERT INTO robot_state (id, command) VALUES (1, 'S');
